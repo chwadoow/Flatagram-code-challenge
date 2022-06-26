51554f7ae89f0f5963c78d5adf32f5897656ae7c
moses ngige <moses32ngigz@gmail.com>
2022-06-26 18:43:04 +0300

    ability to add comments done

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded',fetchData)
 const title = document.getElementById('card-title')
 const image = document.getElementById('card-image')
 const comments = document.querySelectorAll('li')
+list = document.getElementById('comments-list')
 
 function fetchData(){
     fetch('http://localhost:3000/images/1')
@@ -20,18 +21,31 @@ function fetchData(){
     })
 }
 
-document.getElementById('like-button').addEventListener('click',function addLikes(e){
-    e.preventDefault() 
+const likeBtn =document.getElementById('like-button')
+likeBtn.addEventListener('click',likerCount)
+
+function likerCount(event){
+    event.preventDefault() 
     let likes = document.getElementById('like-count')
     let liked=likes.innerText.split(' ')
     let number =  parseInt(liked[0])
     number = number + 1
     likes.innerText= number + ' ' +'likes'
-    
 
-    
+}
+const commentContainer = document.getElementById('comment-form')
+commentContainer.addEventListener('submit', addNewComment)
+const commentValue = document.getElementById('comment')
 
-    
-    
+function addNewComment(event) {
+    event.preventDefault()
+    const newComment = commentValue.value;
+    let newLine =document.createElement('li')
+    newLine.innerText=newComment
+    list.appendChild(newLine)
+    commentContainer.reset()
+   
 
-})
+
+
+}
